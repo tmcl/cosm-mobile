461695fde9a8e393c09b5d9bfd02f8c2a7e934e3
Tristan Mc Leay <tristan@cassowary.org>
2025-12-03 10:12:36 +1100

    fix build

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
       presets: [
-          'babel-preset-expo'
-         , ["@babel/preset-env", {targets: {node: 'current'}}], "@babel/preset-typescript"
+          'babel-preset-expo',
       ],
 
       env: {
